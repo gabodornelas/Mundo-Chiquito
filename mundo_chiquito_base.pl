c4af90cf8225c5ae29bec7b4f687b0f1cb397848
Gabriel De Ornelas <142519693+gabodornelas@users.noreply.github.com>
2025-11-27 11:16:51 -0400

    Add files via upload

---
+++ mundo_chiquito_base.pl
@@ -0,0 +1,12 @@
+:- dynamic mostro/4.
+
+% mostro(nombre, nivel, atributo, poder).
+mostro(mostroUno, 5, luz, 2100).
+mostro(mostroDos, 7, luz, 2400).
+mostro(mostroTres, 7, viento, 2500).
+
+% ternaMundoChiquito(X, Y, Z) :- ...
+
+% mundoChiquito :- ...
+
+% agregarMostro :- ...
