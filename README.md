a95f375e96873dbbb6a2e0a03614d055bed0c48d
gabodornelas <gabodornelas@gmail.com>
2025-11-27 16:26:43 -0400

    README

--- README.md
+++ README.md
@@ -7,4 +7,23 @@
 ---
 
 ## Implementacion
-Aqui va una breve explicacion de la implementacion
+
+# ternaMundoChiquito(X,Y,Z)
+    Termino compuesto que unifica las variables X, Y, Z con nombres de mostros, y NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY, NivelZ, AtributoZ, PoderZ, con el nivel, atributo y poder de cada mostro respectivamente.
+    Se apoya en el termino compuesto compartenUnaCaracteristica para verificar que entre el mostro X y Y comparten solo una caracteristica y entre el mostro Y y Z comparten solo una caracteristica.
+
+# compartenUnaCaracteristica(A1, A2, A3, B1, B2, B3)
+    Termino compuesto que verifica si las variables A1 es igual a B1, A2 es distinto de B2 y A3 es distinto de B3.
+    Siendo A1, A2, A3, B1, B2 y B3 caracteristicas de los mostros, las As son caracteristicas de un mostro las Bs, de otro.
+
+# mundoChiquito()
+    Termino compuesto que encuentra todas las ternas que puede producir ternaMundoChiquito y las guarda en la lista Terna.
+    Luego llama imprimirPorLinea para imprimir las ternas en Terna.
+
+# imprimirPorLinea([[X, Y, Z] | Cola])
+    Predicado que imprime por linea la lista de ternas encontradas en ternaMundoChiquito con caso base y caso recursivo.
+    El caso base es para listas vacias.
+    El caso recursivo toma la cabeza de la lista con los mostros X, Y y Z, los imprime en el formato y llama recusrivamente con la Cola de la lista.
+
+# agregarMostro
+    Termino compuesto que solicita al usuario (con read y format) los datos para un nuevo mostro, da un mensaje donde el usuario verifica los datos del monstruo y luego de la verificaion lo agrega al inicio de la base de conocimientos (con asserta).
--- mundo_chiquito.pl
+++ mundo_chiquito.pl
@@ -5,7 +5,7 @@ mostro(mostroUno, 5, luz, 2100).
 mostro(mostroDos, 7, luz, 2400).
 mostro(mostroTres, 7, viento, 2500).
 
-compartenUnAtributo(A1, A2, A3, B1, B2, B3):-
+compartenUnaCaracteristica(A1, A2, A3, B1, B2, B3):-
     A1 == B1,
     A2 \= B2,
     A3 \= B3.
@@ -16,43 +16,42 @@ ternaMundoChiquito(X, Y, Z):-
     mostro(Z, NivelZ, AtributoZ, PoderZ),
     (
         (   % mostro X y Y SOLO comparten nivel, mostro Y y Z SOLO comparten nivel
-            compartenUnAtributo(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
-            compartenUnAtributo(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
+            compartenUnaCaracteristica(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
+            compartenUnaCaracteristica(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
         );
         (   % mostro X y Y SOLO comparten nivel, mostro Y y Z SOLO comparten atributo
-            compartenUnAtributo(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
-            compartenUnAtributo(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
+            compartenUnaCaracteristica(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
+            compartenUnaCaracteristica(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
         );
         (   % mostro X y Y SOLO comparten nivel, mostro Y y Z SOLO comparten poder
-            compartenUnAtributo(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
-            compartenUnAtributo(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
+            compartenUnaCaracteristica(NivelX, AtributoX, PoderX, NivelY, AtributoY, PoderY),  
+            compartenUnaCaracteristica(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
         );
         (   % mostro X y Y SOLO comparten atributo, mostro Y y Z SOLO comparten nivel
-            compartenUnAtributo(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
-            compartenUnAtributo(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
+            compartenUnaCaracteristica(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
+            compartenUnaCaracteristica(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
         );
         (   % mostro X y Y SOLO comparten atributo, mostro Y y Z SOLO comparten atributo
-            compartenUnAtributo(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
-            compartenUnAtributo(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
+            compartenUnaCaracteristica(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
+            compartenUnaCaracteristica(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
         );
         (   % mostro X y Y SOLO comparten atributo, mostro Y y Z SOLO comparten poder
-            compartenUnAtributo(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
-            compartenUnAtributo(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
+            compartenUnaCaracteristica(AtributoX, NivelX, PoderX, AtributoY, NivelY, PoderY),  
+            compartenUnaCaracteristica(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
         );
         (   % mostro X y Y SOLO comparten poder, mostro Y y Z SOLO comparten nivel
-            compartenUnAtributo(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
-            compartenUnAtributo(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
+            compartenUnaCaracteristica(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
+            compartenUnaCaracteristica(NivelZ, AtributoZ, PoderZ, NivelY, AtributoY, PoderY)
         );
         (   % mostro X y Y SOLO comparten poder, mostro Y y Z SOLO comparten atributo
-            compartenUnAtributo(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
-            compartenUnAtributo(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
+            compartenUnaCaracteristica(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
+            compartenUnaCaracteristica(AtributoZ, NivelZ, PoderZ, AtributoY, NivelY, PoderY)
         );
         (   % mostro X y Y SOLO comparten poder, mostro Y y Z SOLO comparten poder
-            compartenUnAtributo(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
-            compartenUnAtributo(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
+            compartenUnaCaracteristica(PoderX, AtributoX, NivelX, PoderY, AtributoY, NivelY),  
+            compartenUnaCaracteristica(PoderZ, NivelZ, AtributoZ, PoderY, NivelY, AtributoY)
         )
     ).
-
 
 imprimirPorLinea([]).
 
@@ -65,8 +64,7 @@ mundoChiquito:-
     imprimirPorLinea(Terna).
 
 agregarMostro:-
-    format('Vamos a agregar un mostro!!~nUn mostro consiste de nombre, nivel, atributo y poder. Los agregaremos uno por uno~n
-    Ingresa el nombre del mostro (termina con un punto): '),
+    format('Vamos a agregar un mostro!!~nUn mostro consiste de nombre, nivel, atributo y poder. Los agregaremos uno por uno~nIngresa el nombre del mostro (termina con un punto): '),
     read(Nombre),
     write('Ingresa el nivel del mostro (termina con un punto): '),
     read(Nivel),
@@ -74,5 +72,22 @@ agregarMostro:-
     read(Atributo),
     write('Ingresa el poder del mostro (termina con un punto): '),
     read(Poder),
-    asserta(mostro(Nombre, Nivel, Atributo, Poder)),
-    format('Geniail!!, agregaste el mostro "~w" nivel "~w" con atributo "~w" y poder "~w" !~n', [Nombre, Nivel, Atributo, Poder]).
+    format('Geniail!!, agregaste el mostro "~w" nivel "~w" con atributo "~w" y poder "~w"~n', [Nombre, Nivel, Atributo, Poder]),
+    write('Estas seguro de los datos suministrados? Responde con si o no (en minusculas y termina con punto): '),
+    read(Seguro),
+(
+    (
+        Seguro == 'si',
+        asserta(mostro(Nombre, Nivel, Atributo, Poder)),
+        write("Mostro agregado exitosamente :)")
+    );
+    (
+        Seguro == 'no',
+        write("No se agrego el mostro :("), !
+    );
+    (
+        Seguro \= 'si',
+        Seguro \= 'no',
+        write("Esa no fue una opcion valida -.-")
+    )
+).
